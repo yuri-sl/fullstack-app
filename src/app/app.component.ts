edecfbb280c7ddca5c405a52b2642ee30ab0ac15
yuri-sl <yuri03ysl@gmail.com>
2025-08-09 14:50:11 -0300

    main-page-wallpaper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+
+import { InputTextModule } from 'primeng/inputtext'; // se usar pInputText
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenubarModule, ButtonModule, InputTextModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'fullstack-app';
+  shortcutItems = [];
+  actionItems = [
+    { label: 'Configurações', icon: 'pi pi-cog', routerLink: '/config' },
+    { label: 'Ajuda', icon: 'pi pi-question-circle', routerLink: '/ajuda' },
+  ];
 }
