01335b81e44f8d82d68de128494e476d6c2fce4c
yuri-sl <yuri03ysl@gmail.com>
2025-08-23 20:28:29 -0300

    :star: feat: Adição de novos componentes para a página de usuário

---
+++ src/app/components/side-bar/side-bar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-side-bar',
+  imports: [CommonModule],
+  templateUrl: './side-bar.component.html',
+  styleUrl: './side-bar.component.scss',
+})
+export class SideBarComponent {
+  showSideNavBar: boolean = true;
+
+  enableSideBar() {
+    if (this.showSideNavBar) {
+      this.showSideNavBar = false;
+    } else {
+      this.showSideNavBar = true;
+    }
+  }
+}
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  Validators,
+  FormGroup,
+} from '@angular/forms';
+import { UsuarioService } from '../../usuario.service';
 
 @Component({
   selector: 'app-login-page',
-  imports: [ButtonModule],
+  standalone: true,
+  imports: [ButtonModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss',
+  styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  loginForm!: FormGroup; // só declaramos
+
+  loading = false;
+  apiError: string | null = null;
+
+  constructor(private fb: FormBuilder, private usuarioService: UsuarioService) {
+    this.loginForm = this.fb.nonNullable.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(20),
+        ],
+      ],
+    });
+  }
+
+  submit() {
+    this.apiError = null;
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+    this.loading = true;
+    const dadosLogin = this.loginForm.getRawValue();
+    console.log(dadosLogin);
+    this.loading = false;
+  }
+}
